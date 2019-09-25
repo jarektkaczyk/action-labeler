56fd9bcde5b2299dec206fc4ce98a963c0d4d92a
Jarek Tkaczyk <jarek@softonsofa.com>
2019-09-25 21:00:21 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,47 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+
+async function run() {
+  try {
+    const token = core.getInput("repo-token", { required: true });
+
+    await addLabels(new github.GitHub(token));
+  } catch (error) {
+    core.error(error);
+    core.setFailed(error.message);
+  }
+}
+
+async function addLabels(client: github.GitHub) {
+  const prPayload = github.context.payload.pull_request;
+  if (!prPayload) {
+    throw Error("Could not get pull request number from context, exiting");
+  }
+  const { owner, repo } = github.context.repo;
+  const { repository } = await client.graphql(
+    `
+      query PullRequest($owner: String!, $repo: String!, $number: Int!) {
+        repository(owner: $owner, name: $repo) {
+          pullRequest(number: $number) {
+            title
+            number
+            baseRefName
+            headRefName
+          }
+        }
+      }
+      `,
+    {
+      owner: owner,
+      repo: repo,
+      number: prPayload.number
+    }
+  );
+
+  const PR = { ...repository.pullRequest };
+
+  console.log(PR);
+  //   await client.issues.addLabels()
+}
+
+run();
