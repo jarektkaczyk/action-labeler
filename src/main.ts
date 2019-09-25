0e66f66a20004ab70b217ac8b0c8eabcc54a0569
Jarek Tkaczyk <jarek@softonsofa.com>
2019-09-25 22:48:32 +0800

    labels

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,16 @@ import * as github from "@actions/github";
 
 async function run() {
   try {
-    const token = core.getInput("repo-token", { required: true })
-    const prPayload = github.context.payload.pull_request
+    const token = core.getInput("repo-token", { required: true });
+    const prPayload = github.context.payload.pull_request;
     if (!prPayload) {
-      throw Error("Could not get pull request number from context, exiting")
+      throw Error("Could not get pull request number from context, exiting");
     }
 
-    await addLabels(new github.GitHub(token), prPayload)
+    await addLabels(new github.GitHub(token), prPayload);
   } catch (error) {
-    core.error(error)
-    core.setFailed(error.message)
+    core.error(error);
+    core.setFailed(error.message);
   }
 }
 
@@ -35,37 +35,37 @@ async function addLabels(client: github.GitHub, prPayload) {
       repo: github.context.repo.repo,
       number: prPayload.number
     }
-  )
+  );
 
-  const PR = { ...repository.pullRequest }
+  const PR = { ...repository.pullRequest };
 
-  const isWip = PR.title.match(/\bwip\b/i)
-  const isHotfix = PR.headRefName.match(/^hotfix/)
-  const isFeature = PR.headRefName.match(/^feature/)
-  const isRelease = PR.headRefName.match(/^release/)
-  const targetsFeature = PR.baseRefName.match(/^feature/)
+  const isWip = PR.title.match(/\bwip\b/i);
+  const isHotfix = PR.headRefName.match(/^hotfix/);
+  const isFeature = PR.headRefName.match(/^feature/);
+  const isRelease = PR.headRefName.match(/^release/);
+  const targetsFeature = PR.baseRefName.match(/^feature/);
 
-  const labels: string[] = []
+  const labels: string[] = [];
   if (isWip) {
-      labels.push('dnm')
+    labels.push("dnm");
   } else if (isHotfix) {
-      labels.push('hotfix')
+    labels.push("hotfix");
   } else if (isRelease) {
-      labels.push('release')
+    labels.push("release");
   } else if (isFeature && targetsFeature) {
-      labels.push('partial')
+    labels.push("partial");
   } else if (isFeature) {
-      labels.push('feature')
+    labels.push("feature");
   }
 
   if (labels.length > 0) {
     await client.issues.addLabels({
-        // ...github.context.repo,
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-        issue_number: PR.number,
-        labels: labels
-      })
+      // ...github.context.repo,
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      issue_number: PR.number,
+      labels: labels
+    });
   }
 }
 
