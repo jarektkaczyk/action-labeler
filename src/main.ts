7333ced4c9df533eae1b76d888639a41e640bada
Jarek Tkaczyk <jarek@softonsofa.com>
2019-09-25 22:46:21 +0800

    labels

--- src/main.ts
+++ src/main.ts
@@ -3,23 +3,20 @@ import * as github from "@actions/github";
 
 async function run() {
   try {
-    const token = core.getInput("repo-token", { required: true });
+    const token = core.getInput("repo-token", { required: true })
+    const prPayload = github.context.payload.pull_request
+    if (!prPayload) {
+      throw Error("Could not get pull request number from context, exiting")
+    }
 
-    await addLabels(new github.GitHub(token));
+    await addLabels(new github.GitHub(token), prPayload)
   } catch (error) {
-    core.error(error);
-    core.setFailed(error.message);
+    core.error(error)
+    core.setFailed(error.message)
   }
 }
 
-async function addLabels(client: github.GitHub) {
-  console.log(github.context.issue);
-  console.log(github.context.payload.pull_request);
-  const prPayload = github.context.payload.pull_request;
-  if (!prPayload) {
-    throw Error("Could not get pull request number from context, exiting");
-  }
-  const { owner, repo } = github.context.repo;
+async function addLabels(client: github.GitHub, prPayload) {
   const { repository } = await client.graphql(
     `
       query PullRequest($owner: String!, $repo: String!, $number: Int!) {
@@ -34,16 +31,42 @@ async function addLabels(client: github.GitHub) {
       }
       `,
     {
-      owner: owner,
-      repo: repo,
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
       number: prPayload.number
     }
-  );
+  )
 
-  const PR = { ...repository.pullRequest };
+  const PR = { ...repository.pullRequest }
 
-  console.log(PR);
-  //   await client.issues.addLabels()
+  const isWip = PR.title.match(/\bwip\b/i)
+  const isHotfix = PR.headRefName.match(/^hotfix/)
+  const isFeature = PR.headRefName.match(/^feature/)
+  const isRelease = PR.headRefName.match(/^release/)
+  const targetsFeature = PR.baseRefName.match(/^feature/)
+
+  const labels: string[] = []
+  if (isWip) {
+      labels.push('dnm')
+  } else if (isHotfix) {
+      labels.push('hotfix')
+  } else if (isRelease) {
+      labels.push('release')
+  } else if (isFeature && targetsFeature) {
+      labels.push('partial')
+  } else if (isFeature) {
+      labels.push('feature')
+  }
+
+  if (labels.length > 0) {
+    await client.issues.addLabels({
+        // ...github.context.repo,
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        issue_number: PR.number,
+        labels: labels
+      })
+  }
 }
 
 run();
